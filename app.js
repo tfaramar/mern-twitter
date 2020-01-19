31ca2f6a1ff628b76ea3717684507917004b9c67
tfaramarzi <tfaramarzi@gmail.com>
2020-01-19 15:20:00 -0500

    Add route files for users and tweets resources

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true }) //.connect returns a promise
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World pt 2!"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use("/api/users", users);
+// app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
